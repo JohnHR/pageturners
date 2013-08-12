56ae08e679ba96cb045631fb4954b3fa0dde6d79
JohnHR <johnhunter.rogers@gmail>
2013-08-11 23:41:01 -0400

    Working on API...not yet functioning

--- js/someJS.js
+++ js/someJS.js
@@ -1,6 +1,25 @@
 // Page Turner app, initial js file
 // 8/10/13
 
+
+var url1 = 'http://api.harpercollins.com/api/v2/hcapim?apiname=ProductInfo&format=XML&param3=';
+var url2 = '&apikey=yjhac55fhkd2vhe2u5gnz3rc';
+
+// example: helWor("The Old Man and")
+function helWor(title) {
+  title = title || "default";
+  console.log("workin' on it");
+  $.ajax({
+    type: "get",
+    // warning: this part will require some regex to replace spaces with '+'
+    url: url1 + title + url2,
+    success: function (data) {
+      return data;
+      console.log("Data: " + data);
+    }
+  });
+}
+
 $(document).ready(function(){
 	console.log("Dummy js working.");
 	$('#messageWrapper').html('more text! in red!');
