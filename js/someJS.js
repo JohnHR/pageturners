ec865f6dc5ded4fd2a64b4ac5e6d2deace97a8e0
John Rogers <johnhunter.rogers@gmail.com>
2013-08-16 23:39:13 -0400

    Pooooooossssibly working requests

--- js/someJS.js
+++ js/someJS.js
@@ -65,30 +65,50 @@ var url2 = '&apikey=yjhac55fhkd2vhe2u5gnz3rc';
 var fUrl = "http://api.harpercollins.com/api/v3/hcapim?apiname=ProductInfo&format=XML&isbn=9780064410304&apikey=yjhac55fhkd2vhe2u5gnz3rc";
 var fUrl2 = "http://api.harpercollins.com/api/v3/hcapim?apiname=catalog&format=JSON&title=American&apikey=yjhac55fhkd2vhe2u5gnz3rc";
 
-// example: helWor("The Old Man and")
 function getByTitle(title) {
   title = title || "default";
-  console.log("getting by title...");
+  console.log("getting with an AJAX request...");
   $.ajax({
     type: "get",
-    // warning: this part will require some regex to replace spaces with '+'
-    // it's also currently hard-coded and not general at all
-    url: url1 + title + url2,
-    // thought the below would fix the access origin control errors
-    // We faced this is CPMWA a bunch because Chrome is a tight-fisted punk, but it usually worked to run it from
-    //	a web server.  That's not working now, and I'm tapped out for the night.
+    url: fUrl2,
     headers: {
        "X-Requested-With": "XMLHttpRequest",
        "X-Originating-Ip": "127.0.0.1:8000"
     },
     success: function (data) {
-      console.log("Data: " + data);
-      return data;
+      console.log("AJAX load successful.  Returning all books with 'American' in title, in JSON format.");
+      console.log(data);
+      //$('#data').html(data);
     }
+  });
+}
+
+function basicGet() {
+  console.log("getting with an HTTP request...");
+  $.get(fUrl, function(data) {
+    //$('.result').html(data);
+    console.log('HTTP load was performed. Returning book with ISBN 9780064410304, in XML format.');
+    console.log(data);
+    //$('#data').html(data);
   });
 }
 
 $(document).ready(function(){
 	console.log("Dummy js working.");
 	$('#messageWrapper').html('more text! in red!');
+  $('#ajax').click( getByTitle );
+  $('#http').click( basicGet );
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
